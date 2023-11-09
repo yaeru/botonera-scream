13891f0861021f05c29fd20d10a3884261046ae6
Yael Duckwen <yaelduckwen@gmail.com>
2023-11-09 14:17:13 -0300

    init

---
+++ nuxt.config.js
@@ -0,0 +1,59 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'BotoneraScream',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend(config, { isDev, isClient }) {
+      // Añadir configuración para loaders
+      config.module.rules.push({
+        test: /\.(mp3|ogg)$/i, // Agrega los formatos de archivo de audio que quieres manejar
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // Si el archivo es más grande, se manejará con file-loader
+              name: 'audio/[name].[ext]'
+            }
+          }
+        ]
+      });
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<h1>
+			Botonera
+		</h1>
+
+		<div class="grid">
+			<button @click="playAudio('quotes/01.ogg')">Reproducir Audio 1</button>
+			<button @click="playAudio('quotes/02.ogg')">Reproducir Audio 2</button>
+			<button @click="playAudio('quotes/03.ogg')">Reproducir Audio 3</button>
+			<button @click="playAudio('quotes/04.ogg')">Reproducir Audio 4</button>
+		</div>
+	</div>
+</template>
+<style type="text/css">
+	.grid {
+		display: grid;
+		gap: 20px;
+		grid-template-columns: repeat(4, 1fr);
+	}
+</style>
+
+<script>
+	export default {
+		data() {
+			return {
+				audio: null
+			};
+		},
+		methods: {
+			playAudio(audioSrc) {
+				if (this.audio) {
+        // Detener la reproducción si ya hay un audio reproduciéndose
+					this.audio.pause();
+				}
+
+      // Crear una nueva instancia del objeto de audio
+				this.audio = new Audio(audioSrc);
+
+      // Reproducir el nuevo audio
+				this.audio.play();
+			}
+		}
+	}
+</script>
