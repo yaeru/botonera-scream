8b0f498369c92bd1420b683063fa810b1c23abd5
Yael Duckwen <yaelduckwen@gmail.com>
2023-12-06 18:19:25 -0300

    3e89eba

---
+++ _nuxt/static/1701897558/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbotonera-scream\u002F_nuxt\u002Fstatic\u002F1701897558",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbotonera-scream\u002F",assetsPath:"\u002Fbotonera-scream\u002F_nuxt\u002F",cdnURL:a}}}}(null));
